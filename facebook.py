514b231f1afb356727b6e1a281e25d9d1723d3e6
AakritiGupta16 <59439108+AakritiGupta16@users.noreply.github.com>
2023-05-27 12:16:36 +0530

    Add files via upload

---
+++ facebook.py
@@ -0,0 +1,18 @@
+import facebook
+import urllib3
+import requests
+
+import os
+import requests
+from facebook_scraper import get_profile
+
+
+# def scrape_facebook_profile(str):
+get_profile('Aakriti1973')
+        # """scrape information from Facebook profiles,
+        #     Manually scrape the information from the Facebook profile"""
+
+
+# posts = scrape_facebook_profile("https://www.facebook.com/Aakriti1973")
+# print(posts)
+
